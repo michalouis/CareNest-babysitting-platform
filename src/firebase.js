94474b2476c00a545b7c4f0ef6d8f5d46a1336b0
michalouis <michael.aretakis2002@gmail.com>
2025-03-29 12:46:35 +0200

    minor changes

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import {initializeApp} from "firebase/app";
+import {browserLocalPersistence} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import { initializeAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: browserLocalPersistence,
+});
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
